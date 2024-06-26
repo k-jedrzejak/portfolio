0759b8385c83d4d22c22cbd0cb2a0eb8d7e54690
k-jedrzejak <kjedrzejak91@gmail.com>
2024-06-26 12:31:02 +0200

    add github action file

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '../views/AboutView.vue';
 import NProgress from 'nprogress';
 import { removeBodyClass, addBodyClass} from '@/utils';
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  linkExactActiveClass: 'active',
-  routes: [
+  history: createWebHistory(process.env.BASE_URL),
+  linkExactActiveClass: "active",
+  routes:  [
     {
       path: '/',
-      name: '.home',
-      component: HomeView
+      name: '.all',
+      component: HomeView,
     },
     {
       path: '/about',
       name: '.about',
-      component: AboutView
+      component: AboutView,
     }
   ]
-});
+})
 
 // progress bar
 NProgress.configure({
